1566811df6426b3729d433f62b766dec06ef8b13
KenanBiren <116853630+KenanBiren@users.noreply.github.com>
2022-10-29 11:41:51 -0700

    Delete weekly_links.py
    Accidentally uploaded weekly_links.py when code is complete and uploaded already (weekly_scrape.py)

--- src/backend/process_scrape_and_analysis/weekly_links.py
+++
@@ -1,4 +0,0 @@
-This script runs weekly (when "Week of XXXX/XX/XX" folders are formatted).
-It scrapes the full item list from the wiki database and compares its length
-to the data stored locally. If new items are detected, new_items.csv is edited
-to indicate such (leading to new_item_trigger = True in process_backend.py)
