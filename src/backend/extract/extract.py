04e4eff392cdd35d7c7aff2cca5e2333cfcdcf6a
KenanBiren <116853630+KenanBiren@users.noreply.github.com>
2022-11-04 22:17:45 -0700

    Update extract.py

--- src/backend/extract/extract.py
+++ src/backend/extract/extract.py
@@ -13,13 +13,13 @@ with open("data/full_or_incr.csv", 'r') as f:
     f.close()
 
 if trigger == 'full':            
-    os.system('cd code/src/extract/full_scrape/')
+    os.system('cd src/extract/full_scrape/')
     os.system("scrapy crawl full_scrape -o full_output.csv")
     # add unit test here
     os.system("python3 post_scrape.py")
     # add unit test here
 else:
-    os.system('cd code/src/extract/incr_scrape/')
+    os.system('cd src/extract/incr_scrape/')
     os.system("scrapy crawl incr_scrape -o incr_output.csv")
     # add unit test here
     os.system("python3 post_scrape.py")
